ae4e101238c1016c7ca173e05f8c7cba564b1916
phenix <sergio.lv2005@gmail.com>
2025-12-19 17:22:21 +0100

    arreglo de errores treesitter

--- lua/plugins/coc-nvim_conf.lua
+++
@@ -1,4 +0,0 @@
-vim.o.updatetime = 100 -- tiempo antes de comprobacion (para que no se quede tostado)
-vim.o.pumheight = 20 -- maximo de altura de ventana emergente
--- keymap 
-vim.api.nvim_set_keymap('i', '<TAB>', [[pumvisible() ? coc#_select_confirm() : coc#expandableOrJumpable() ? "\<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump',''])\<CR>" : v:lua.check_back_space() ? "\<TAB>" : coc#refresh()]], {expr = true, silent = true})
