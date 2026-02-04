31ec028928d559378f8c8142b61eab56d39e625f
default <sergio.lopezventura@educa.madrid.org>
2026-02-04 10:51:30 +0000

    dbee completo

--- lua/keymap/dbee_keymap.lua
+++ lua/keymap/dbee_keymap.lua
@@ -1,7 +1,6 @@
 local map = vim.keymap.set
+-- No se que keymap poner
+-- map("n", "<leader>op", "<cmd>Dbee open<cr>", { desc = "Abrir DBee" })
+-- map("n", "<leader>cl", "<cmd>Dbee close<cr>", { desc = "Cerrar DBee" })
 
--- Abrir/Cerrar la interfaz completa
-map("n", "<leader>do", "<cmd>Dbee open<cr>", { desc = "Abrir DBee" })
-map("n", "<leader>dc", "<cmd>Dbee close<cr>", { desc = "Cerrar DBee" })
-
-map("n", "<leader>de", "<cmd>Dbee execute<cr>", { desc = "Ejecutar Query" })
+map("n", "<leader>BB", "<cmd>Dbee execute<cr>", { desc = "Ejecutar Query" })
--- lua/plugins/dbee_conf.lua
+++ lua/plugins/dbee_conf.lua
@@ -1,26 +1,22 @@
--- lua/plugins/dbee_conf.lua
 return {
-  "kndndrj/nvim-dbee",
+"kndndrj/nvim-dbee",
   dependencies = {
     "MunifTanjim/nui.nvim",
   },
+  build = function()
+    require("dbee").install()
+  end,
   config = function()
-    local dbee = require("dbee")
-    
-    -- Intentamos cargar fuentes, si falla usamos tabla vacía
-    local sources_ok, my_sources = pcall(require, "databases.db_sources")
-    
-    dbee.setup({
-      sources = sources_ok and my_sources or {},
+    require("dbee").setup({
+      sources = {
+        require("dbee.sources").MemorySource:new({
+          {
+            name = "el_coder", 
+	    type = "mysql",   
+            url = "usuario:usuario@tcp(localhost:3306)/miscosas",
+          },
+        }),
+      },
     })
-
-    vim.api.nvim_create_user_command("Dbee", function()
-      -- El primer pcall "limpia" el error de inicialización en silencio
-      pcall(dbee.open)
-      -- El defer asegura que si falló el primero, el segundo abra la ventana
-      vim.defer_fn(function()
-        dbee.open()
-      end, 50)
-    end, {})
   end,
 }
